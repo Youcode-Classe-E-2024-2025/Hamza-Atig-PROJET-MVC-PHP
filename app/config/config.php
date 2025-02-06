856e26ae930993b763834e69cc6e2ad998323e5b
Atig-Hamza <hamzaatig@icloud.com>
2025-02-06 13:00:55 +0100

    setup main db config

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => '5432',
+        'dbname' => 'nom_de_ta_base',
+        'user' => 'utilisateur',
+        'password' => 'mot_de_passe',
+    ],
+        'app' => [
+            'base_url' => '/Hamza-Atig-PROJET-MVC-PHP/public'
+        ]
+];
+
