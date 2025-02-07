33983b44a4de5c07674ed031d6e7e9d9ce9214c3
Atig-Hamza <hamzaatig@icloud.com>
2025-02-07 09:15:06 +0100

    simple changes on login.twig

--- app/controllers/front/HomeController.php
+++ app/controllers/front/HomeController.php
@@ -5,6 +5,6 @@ use App\Core\Controller;
 use App\Core\View; 
 class HomeController extends Controller {
     public function index() {
-        View::render('front/signup.twig', ['message' => 'Bienvenue sur la page d\'accueil!']);
+        View::render('front/login.twig', ['message' => 'Bienvenue sur la page d\'accueil!']);
     }
 }
